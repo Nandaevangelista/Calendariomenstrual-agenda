715059c0bcfa515a379dec00ceb966706148a09b
Nanda Evangelista <99517819+Nandaevangelista@users.noreply.github.com>
2022-03-22 21:41:53 -0300

    calendario-2022

---
+++ janeiro/regular.js
@@ -0,0 +1,11 @@
+let body = document.querySelector(`body`) 
+let h1 = document.createElement(`h1`);
+h1.innerHTML = `Calendário Menstrual`;
+body.appendChild(h1)
+
+//console.log(calendario);
+
+let table = document.createElement(`table`);
+let thead = document.createElement(`thead`);
+let tr = document.createElement(`tr`);
+let td = document.createElement(`td`);
