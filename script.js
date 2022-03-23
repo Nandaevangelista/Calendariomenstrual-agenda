d2c4eea8ec2caca766b348bec5179045d6b0aa54
Nanda Evangelista <99517819+Nandaevangelista@users.noreply.github.com>
2022-03-22 21:37:11 -0300

    inicial

---
+++ script.js
@@ -0,0 +1,19 @@
+alert(`Bem vindo ao seu Planner Online!`)
+var nome = window.prompt('Qual é o seu nome?')
+    usuario.innerHTML = `Olá, <strong>${nome}!`
+
+function verificar() {
+    let fciclo = document.getElementsByName(`ciclo`)
+    let res = document.querySelector(`div#res`)
+    let genero = ``
+    if (fciclo[0].checked){
+        genero = `Regular` 
+        
+    } else if(fciclo[1].checked) {
+        genero = `Irregular`
+    } 
+res.innerHTML = `Seu ciclo é ${genero}.` 
+}
+
+
+
