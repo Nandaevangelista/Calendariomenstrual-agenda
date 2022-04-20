00c730e6171f52b59498bfc8d7ab299c5867a9c7
Nanda Evangelista <99517819+Nandaevangelista@users.noreply.github.com>
2022-04-20 17:25:33 -0300

    Commit
    Atualização com mudanças necessárias

--- script.js
+++ script.js
@@ -1,14 +1,16 @@
-alert(`Bem vindo ao seu Planner Online!`)
+//alert(`Bem vindo ao seu Planner Online!`)
 var nome = window.prompt('Qual é o seu nome?')
-    usuario.innerHTML = `Olá, <strong>${nome}!`
+    usuario.innerHTML = `Olá, <strong>${nome}!`//
 
+    
+    
 function verificar() {
     let fciclo = document.getElementsByName(`ciclo`)
     let res = document.querySelector(`div#res`)
     let genero = ``
     if (fciclo[0].checked){
         genero = `Regular` 
-        
+         
         
     } else if(fciclo[1].checked) {
         genero = `Irregular`
